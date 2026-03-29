794f78ecf6631eb55939ec9de566de5e7130284e
mauriciopintos <mauricioenrique.pintos@estudiantes.unahur.edu.ar>
2026-03-29 19:38:57 -0300

    Corrijo comisiones 2026C1+

--- src/paths.js
+++ src/paths.js
@@ -1,6 +1,7 @@
 export const PATH = '/'
+export const PATH_CURSADA_JUEVES = `/comision-1-2-jueves`
 export const PATH_CURSADA_VIERNES = `/comision-3-viernes`
-export const PATH_CURSADA2025c1_MIERCOLES = `/comision-1-2-jueves`
+export const PATH_CURSADA2025c1 = `/cursada2025c1`
 export const PATH_CONTRATO_PEDAGOGICO = `/contrato-pedagogico`
 export const PATH_VIDEOS = `/videos`
 export const PATH_MATERIAL = `/material`
